7feb4d8274d03b6e52637d3989ef5089c5fc58e9
KhooTeckWei <khootw-sm17@student.tarc.edu.my>
2020-01-12 04:38:23 +0800

    sleep

---
+++ app/src/main/java/com/example/thumbupdown/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.thumbupdown
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
